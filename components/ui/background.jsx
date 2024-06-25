596b6f749462eae604193da737676ca339a3c7ce
Harshal Pagare <frontend@eight-at-521574992517>
2024-06-25 22:36:13 +0530

    a

--- components/ui/background.jsx
+++ components/ui/background.jsx
@@ -80,7 +80,7 @@ export function GridSmallBackground({ children }) {
   return (
     <div className=" w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black/[0.6] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="p-24 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+      <div className=" bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
         {children}
       </div>
     </div>
